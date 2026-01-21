f6665389f076733acbf2fe0c093013f6838ec440
ilham <frandly26@gmail.com>
2026-01-22 00:03:22 +0700

    menambahkan link pada href

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded',()=>{
+
+    window.addEventListener('beforeunload', () => {
+        window.scrollTo(0, 0);
+    });
     
-    // btn ham menu 
     document.querySelectorAll('.nav-btn').forEach(function(btn){
         btn.addEventListener('click', function(){
             navDashboard.classList.toggle('nav-dashboard-active');
@@ -18,6 +21,60 @@ document.addEventListener('DOMContentLoaded',()=>{
             navDashboard.classList.remove('nav-dashboard-active');
         }
     });
-    
+
+    const navbarLinks = document.querySelectorAll(".nav-dashboard-link a[href^='#']");
+    navbarLinks.forEach(function(link){
+        link.addEventListener('click', function(e){
+            e.preventDefault();
+            const linkHref = link.getAttribute('href');
+            const targerElement = document.querySelector(linkHref);
+            if (targerElement) {
+                const navbar = document.querySelector('.nav');
+                const navbarHeight = navbar.clientHeight;
+                const ElementTop = targerElement.getBoundingClientRect().top + window.scrollY;
+                window.scrollTo({
+                    top:ElementTop - navbarHeight,
+                    behavior:'smooth'
+                });
+            }
+        });
+    });
+
+    const footerLinks = document.querySelectorAll(".footer-links a[href^='#']");    
+    footerLinks.forEach(function(link){
+        link.addEventListener('click', function(e){
+            e.preventDefault();
+            const linkHref = link.getAttribute('href');
+            const targerElement = document.querySelector(linkHref);
+            if (targerElement) {
+                const navbar = document.querySelector('.nav');
+                const navbarHeight = navbar.clientHeight;
+                const ElementTop = targerElement.getBoundingClientRect().top + window.scrollY;
+                window.scrollTo({
+                    top:ElementTop - navbarHeight,
+                    behavior:'smooth'
+                });
+            }
+        });
+    });
+
+
+    const btnShare = document.querySelector('.share');
+    btnShare.addEventListener('click', async(e)=>{
+        e.preventDefault();
+        if (navigator.share) {
+            try {
+            await navigator.share({
+                title: 'My Portfolio',
+                text: 'Check out my website!',
+                url: window.location.href
+            });
+            } catch (error) {
+            console.log('Share cancelled', error);
+            }
+        } else {
+            console.log('Share cancelled', error);
+        }
+    });
 
 });
